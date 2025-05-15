f3d8742d643ffda49701e54cbc8720981755799c
rensys <39058702+NikitaKolotushkin@users.noreply.github.com>
2025-05-16 01:28:48 +0300

    add: new fields in Events models; new models

--- ryadom_common/ryadom_common/schemas/events.py
+++ ryadom_common/ryadom_common/schemas/events.py
@@ -1,6 +1,21 @@
+from datetime import datetime
 from pydantic import BaseModel
+from typing import List
 
 
-class Event(BaseModel):
+class EventBase(BaseModel):
     id: int
     name: str
+    description: str | None = None
+
+
+class EventCreate(EventBase):
+    pass
+
+
+class EventResponse(EventBase):
+    created_at: datetime
+
+
+class EventListResponse(BaseModel):
+    events: List[EventResponse]
--- ryadom_common/setup.py
+++ ryadom_common/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ryadom_common",
-    version="0.0.1",
+    version="0.0.2",
     packages=["ryadom_common"],
     package_dir={"": "."},
 )
