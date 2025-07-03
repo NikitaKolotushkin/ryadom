31ca4077cc706a6a84e46b862433762a48b01a3c
NikitaKolotushkin <nikitaplay1000@gmail.com>
2025-07-03 22:43:10 +0400

    fix: environments instead of ports

--- ryadom_edge-router/start.sh
+++ ryadom_edge-router/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-uvicorn app.main:app --host 0.0.0.0 --port 8080 --reload
+uvicorn app.main:app --host 0.0.0.0 --port ${EDGE_ROUTER_SERVICE_PORT} --reload
--- ryadom_front-end/start.sh
+++ ryadom_front-end/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-uvicorn app.main:app --host 0.0.0.0 --port 8081 --reload
+uvicorn app.main:app --host 0.0.0.0 --port ${FRONT_END_SERVICE_PORT} --reload
