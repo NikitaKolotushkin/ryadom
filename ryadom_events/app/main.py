180a8757982275a6e8dbfe6f268f2a603ed204c5
NikitaKolotushkin <nikitaplay1000@gmail.com>
2025-07-04 20:40:42 +0400

    add: db table creating on startup

--- ryadom_events/app/main.py
+++ ryadom_events/app/main.py
@@ -3,8 +3,18 @@
 
 from fastapi import FastAPI
 
+from app.database import engine
 from app.routes.routes import router
+from app.models.event import Base
 
 
 app = FastAPI()
+
+
+@app.on_event('startup')
+async def startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
 app.include_router(router)
