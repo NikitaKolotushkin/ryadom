34ab1b020a0c1d96c193f1ab72925ee18a76562a
NikitaKolotushkin <nikitaplay1000@gmail.com>
2025-07-09 22:17:19 +0400

    add: mount static directory

--- ryadom_front-end/app/main.py
+++ ryadom_front-end/app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from app.services.front_end_service import router
 
 
 app = FastAPI()
+
+app.mount('/static', StaticFiles(directory='app/static'), name='static')
+
 app.include_router(router)
