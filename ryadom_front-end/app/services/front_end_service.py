2536c983ec0ff14f3493e06809d39e31b8c32fa1
rensys <39058702+NikitaKolotushkin@users.noreply.github.com>
2025-07-12 00:20:59 +0400

    add: new URL paths

--- ryadom_front-end/app/services/front_end_service.py
+++ ryadom_front-end/app/services/front_end_service.py
@@ -10,3 +10,18 @@ templates = Jinja2Templates(directory="app/templates")
 @router.get("/", response_class=HTMLResponse)
 async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@router.get("/event/", response_class=HTMLResponse)
+async def users(request: Request):
+    return templates.TemplateResponse("event.html", {"request": request})
+
+
+@router.get("/login/", response_class=HTMLResponse)
+async def users(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+
+@router.get("/register/", response_class=HTMLResponse)
+async def users(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
