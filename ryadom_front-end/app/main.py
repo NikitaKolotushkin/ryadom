98629e68c419367f72fdc5ef8cb6169a4f404930
rensys <39058702+NikitaKolotushkin@users.noreply.github.com>
2025-07-29 17:33:31 +0400

    add: redirecting from 404 to /

--- ryadom_front-end/app/main.py
+++ ryadom_front-end/app/main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 
 from app.routes.routes import router
@@ -12,3 +13,8 @@ app = FastAPI()
 app.mount('/static', StaticFiles(directory='app/static'), name='static')
 
 app.include_router(router)
+
+
+@app.exception_handler(404)
+async def redirect_404(request: Request, exc):
+    return RedirectResponse(url="/")
