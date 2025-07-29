ce42a6c627679f0d55647d9094473a980ff5399c
rensys <39058702+NikitaKolotushkin@users.noreply.github.com>
2025-07-29 13:28:15 +0400

    add: new fields in psql init script

--- ryadom_events/db/init-events-db.sql
+++ ryadom_events/db/init-events-db.sql
@@ -1,22 +1,26 @@
 -- EVENTS
 CREATE TABLE IF NOT EXISTS event (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    url TEXT NOT NULL,
+    category TEXT,
+    format TEXT NOT NULL DEFAULT 'online',
+    name TEXT NOT NULL,
     description TEXT,
-    photo VARCHAR(255),
-    location VARCHAR(255),
-    date VARCHAR(255) NOT NULL,
-    -- ends_at TIMESTAMP NOT NULL,
+    photo TEXT,
+    banner TEXT,
+    location TEXT,
+    address TEXT,
+    date TEXT NOT NULL,
     max_participants INTEGER,
-    color VARCHAR(16),
-    created_at VARCHAR(64)
+    color TEXT,
+    created_at TEXT NOT NULL
 );
 
 -- MEMBERS
 CREATE TABLE IF NOT EXISTS member (
     id SERIAL PRIMARY KEY,
     event_id INTEGER NOT NULL REFERENCES event(id) ON DELETE CASCADE,
-    user_id VARCHAR(255) NOT NULL,
-    role VARCHAR(50) DEFAULT 'participant',
+    user_id TEXT NOT NULL,
+    role TEXT DEFAULT 'participant',
     UNIQUE(event_id, user_id)
 );
