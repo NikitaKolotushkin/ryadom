d2cf909b9953f1fce1579be1974e70b36eab6a32
rensys <39058702+NikitaKolotushkin@users.noreply.github.com>
2025-08-02 02:27:36 +0400

    add: import new base

--- ryadom_events/app/models/event.py
+++ ryadom_events/app/models/event.py
@@ -1,8 +1,6 @@
 from sqlalchemy import Column, Integer, String, Text, DateTime
-from sqlalchemy.ext.declarative import declarative_base
 
-
-Base = declarative_base()
+from app.models.base import Base
 
 
 class EventModel(Base):
