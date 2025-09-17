b3a8ec319970c1176a17bff2858ebc7fba67226d
rensys <39058702+NikitaKolotushkin@users.noreply.github.com>
2025-09-17 15:05:19 +0300

    feat(users): Общая база для моделей user & refresh_token

--- ryadom_users/app/database.py
+++ ryadom_users/app/database.py
@@ -1,15 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import os
 
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from typing import AsyncGenerator
-from app.models.user import Base
 
+
+Base = declarative_base()
 
 DATABASE_URL = os.getenv("POSTGRES_USERS_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+from app.models.user import UserModel
+from app.models.refresh_token import RefreshTokenModel
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
