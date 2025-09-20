547b15ec9845151a250d4d615505f8d168d42ae8
rensys <39058702+NikitaKolotushkin@users.noreply.github.com>
2025-09-20 14:41:33 +0300

    feat(edge-router): Добавлен CORS middleware для обмена запросами между микросервисами

--- ryadom_edge-router/app/main.py
+++ ryadom_edge-router/app/main.py
@@ -3,11 +3,26 @@
 
 from fastapi import FastAPI
 
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.config import get_config
 from app.routes.routes import router
 
 
 config = get_config()
 
-app = FastAPI(docs_url=config.DOCS_URL, redoc_url=config.REDOC_URL, openapi_url=config.OPENAPI_URL)
+app = FastAPI(docs_url=config.DOCS_URL, redoc_url=config.REDOC_URL, openapi_url=config.OPENAPI_URL, redirect_slashes=False)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        'http://localhost:8081',    # Dev front-end
+        'https://ryadom-spbu.ru'    # Prod front-end
+    ],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+    max_age=3600
+)
+
 app.include_router(router, prefix=config.API_PREFIX)
